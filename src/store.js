a25b4ca4b24baef0f607b931ebde387be197ebbb
Erick Otenyo <otenyo.erick@gmail.com>
2018-11-06 11:03:17 +0300

    fix redux devtools extension

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ const reducer = combineReducers({
 
 const store = createStore(
 	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
